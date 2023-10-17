c25050f6d48850a19cecb54d6a99c93de1f83cf8
Federico-Maiocchi <maiocchifederico@gmail.com>
2023-10-17 19:48:44 +0200

    ADD: readme

--- README.md
+++ README.md
@@ -8,8 +8,15 @@
 - stampo un alert dove dico al giocatore di memorizzarsi i 5 numeri, dentro l'altre metterò la varibile dell'array
 
 - far partire un timer di 30 secondi
+    - creeremo una funzione che ci permetterà di fare un conto da 0 a 30 secondi (non sarà visibile a all'utente)
 
 - l'utente deve inserire i numeri precedenti uno alla volta tramite prompt
+- faremo una funzione ci da modo di
+    - chiedere all'utente un numero massimo di volte i numeri
+    - reciclare i suoi numeri nell'array
+    - controllare se ci sono numeri uguali
+        - SE ci sono alora verrano tolti. (non considerati)
 
-- dopo l'inserimento dei 5 numeri:
-    - il software indicherà quanti e quali numeri sono stati individuati
+- confontiamo i numeri random con i numeri dell'utente
+    - SE ci sono dei numeri uguali
+        - stamperemo le parole e i numeri visibile all'utente
--- js/app.js
+++ js/app.js
@@ -1,29 +1,6 @@
-// console.log('hello');
-
-// inizializziamo una varibile dove invocheremo la funzione getArrayOfRandomIntBetween
-// i numeri generati partiranno da 1 a 100 e saranno 5 
-const numberRandom = getArrayOfRandomIntBetween(1, 100, 5);
-// console.log(numberRandom);
-
-// creo un alert per far visualizzare i numeri all'utente aggiugnendo la variabile numberRandom 
-alert('Memorizza i seguenti numeri: ' + numberRandom)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // FUNCTION
 
-// funzione per generare numeri random con un numero minmo e uno massimo
+// Funzione per generare numeri random con un numero minmo e uno massimo
 function getRandomIntInclusive(min, max) {
 	min = Math.ceil(min);
 	max = Math.floor(max);
@@ -54,3 +31,97 @@ function getArrayOfRandomIntBetween(minRange, maxRange, number) {
     // return array di numeri generati
     return numbersArray
 }
+
+// Funzione per fermare il conteggio
+function tempoScaduto() {
+	console.log('Il tempo è scaduto!')
+
+   
+
+	// interrompiano l'interval
+	clearInterval(idInterval)
+}
+
+// Funzione per ricavari i numeri digitati dall'utente
+function numberInsertUser() {
+    
+    // ciclo for che mi permette di ciclare i prompt dell'utente, dove indico le volte che chiederò all'utente i numeri cioè 5
+    for (let i = 0; i < 5; i++) {
+        
+        // converto la stringa e e ciclo i prompt
+        const numbersUser = parseInt(prompt('quali sono i numeri?'))
+        // console.log(numbersUser);
+
+        // serve a cancellare i numeri dobbi,verranno considerati solo numeri unici
+        if (!userArray.includes(numbersUser)) {
+
+            // i numeri verranno inseriti dentro l'array utente
+            userArray.push(numbersUser);
+
+            // se il numero del pc (random) è uguale ai numeri digitati dall'utente 
+            if (numberRandom.includes(numbersUser)) {
+
+                // aumento il punteggio dell'untente
+                scoreUser++;
+
+                // indicherò all'utente il suo punteggio indicando i numeri che ha indovinato
+                const scoreTot = `hai memorizzato ${scoreUser} : ${userArray}` 
+                
+                // elemento presa dal Dom è usuale al totale (punteggio utente) (lista numeri utente )
+                score.innerHTML = scoreTot;
+
+            }
+        } 
+
+        console.log(numbersUser);
+    }
+}
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// console.log('hello');
+
+// inizializziamo una varibile dove invocheremo la funzione getArrayOfRandomIntBetween
+// i numeri generati partiranno da 1 a 100 e saranno 5 
+const numberRandom = getArrayOfRandomIntBetween(1, 100, 5);
+console.log(numberRandom);
+
+// creo un alert per far visualizzare i numeri all'utente aggiugnendo la variabile numberRandom 
+alert('Memorizza i seguenti numeri: ' + numberRandom );
+
+// dichiaro un array vuoto dove inserirò dentro i numeri digitati dall'utente
+let userArray = []
+
+// avvio un conteggio che parte da 1 a 30 secondi e poi si stoppa il conteggio
+setTimeout (numberInsertUser, 30000);
+console.log(userArray)
+
+// dichiaro una variabile del punteggio utente uguale a zero
+let scoreUser = 0;
+
+// prendo dal dom la classe score all'interno del div
+let score = document.querySelector('.score');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
